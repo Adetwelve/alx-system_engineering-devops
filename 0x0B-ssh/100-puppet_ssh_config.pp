acf347c765091a96b2fd410d5f0d01ff9433880f
Adetwelve <niyero10@gmail.com>
2023-03-27 00:25:04 +0000

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#!/usr/bin/puppet
+# configure ssh with Puppet
+
+include stdlib
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
+}
