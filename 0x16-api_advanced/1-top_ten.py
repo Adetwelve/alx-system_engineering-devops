6ce3f36261db26b91cba9db45b7a3e192cb37f90
Adetwelve <niyero10@gmail.com>
2023-05-11 17:41:45 +0000

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and 
+"""function that queries the Reddit API and
     prints the titles of the first 10 hot posts
 """
 import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limiti=10".format(subreddit)
     headers = {'User-Agent': 'niyi'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 200:
         """valid response"""
         response = response.json()
@@ -18,4 +18,3 @@ def top_ten(subreddit):
             print(post.get('data').get('title'))
     """invalide response"""
     print(None)
-
