f7513ff50b47b717477d5d6140321c4bda5e13a9
Adetwelve <niyero10@gmail.com>
2023-05-12 21:16:48 +0000

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# A script that replace strings ".phpp" with ".php" extension.
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%= File.read("/var/www/html/wp-settings.php").gsub(/\.phpp/, ".php") %>'),
